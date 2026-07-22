5187f786dbde33cb35f435fbf4ddceb65210624a
BalrajSinghGidda <anonystargamerz@gmail.com>
2026-07-22 13:49:32 +0530

    plymouth

--- modules/nixos/core.nix
+++ modules/nixos/core.nix
@@ -1,11 +1,30 @@
-{ pkgs, ... }: {
-  # Bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  #boot.kernelPackages = pkgs.linuxPackages_latest;
+{pkgs, ...}: {
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+    plymouth = {
+      enable = true;
+      theme = "hexagon_hud";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = ["hexagon_hud"];
+        })
+      ];
+    };
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "rd.udev.log_level=3"
+      "rd.systemd.show_status=auto"
+    ];
+    loader.timeout = 0;
+  };
 
   # Nix Settings
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   # Timezone and Locale
   time.timeZone = "Asia/Kolkata"; # Replace with your timezone if needed
@@ -15,11 +34,11 @@
   users.users.balraj = {
     isNormalUser = true;
     description = "balraj";
-    extraGroups = [ "networkmanager" "wheel" "video" ];
+    extraGroups = ["networkmanager" "wheel" "video"];
     shell = pkgs.nushell; # Set Nushell as default system shell
   };
 
-  environment.shells = with pkgs; [ nushell ];
+  environment.shells = with pkgs; [nushell];
 
   # Networking
   networking.networkmanager.enable = true;
